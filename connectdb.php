8dd5adfdad31a170476c3961e456c7fb4bc4d4b5
decorix <polevg2015@yandex.ru>
2022-12-20 00:35:12 +0300

    2

--- connectdb.php
+++ connectdb.php
@@ -2,6 +2,6 @@
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', '');
+define('DB_NAME', 'lab4');
 $mysql = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 ?>
--- index.php
+++ index.php
@@ -1,93 +1,42 @@
 <?php
 
 include 'header.html';
+include 'connectdb.php';
+$result = mysqli_query($mysql, 'SELECT * FROM `lab4`');
+$result2 = mysqli_query($mysql, 'SELECT * FROM `lab4`');
 
 ?>
 <html>
     <body>
-    <div class="container">
-        <div class="h1_container">
-            <h1>Форма обратной связи</h1>
+        <div class="container">
+            <div class="container_img">
+            <?php 
+                while($name = mysqli_fetch_assoc($result)){
+            ?>
+                <img src="<?php echo $name['img']; ?>" alt="<?php echo $name['name_img'];?>" width="200px">
+                <?php
+            }
+            ?>
+            </div>
+        <table class="table">
+            <tr>
+                <th>Термин</th>
+                <th>Описание</th>
+            </tr>
+            <?php 
+                while($name = mysqli_fetch_assoc($result2)){
+            ?>
+                <tr>
+                <td><?php echo $name['term'];?></td>
+                <td><?php echo $name['explanation'];?></td>  
+                </tr>
+            <?php
+            }
+            ?>
+        </table>
+        
         </div>
-        <div class="form_container">
-            <form action="home.php" method="post">
-                <div class="form-group">
-                    <label class="form-label" for="name">ФИО</label>
-                    <input class="inputs" id="name" type="text" name="name"  required value=<?php
-                    if (isset($_GET['NAME'])) {
-                        echo $_GET['NAME'];
-                    } else {
-                        echo '';
-                    }
-                    ?>>
-                </div>
-
-                <div class="form-group">
-                    <label class="form-label" for="email">Email</label>
-                    <input class="inputs" id="email" type="email" name="email" placeholder="Введите email" required value=<?php
-                    if(isset($_GET["EMAIL"])){
-                            echo $_GET["EMAIL"];
-                        } else {
-                            echo "";
-                        }
-                    ?>>
-                </div>
-
-                <div class="form-group">
-                    <p class="form-label">Как вы узнали о нас?</p>
-                    <input type="radio" name="source" id="marketing" value="marketing" 
-                    <?php
-                         if(isset($_GET['E']) && $_GET['E'] == 'marketing') {
-                        echo 'checked';
-                         }
-                    ?>/>
-                    <label for="marketing">Реклама в интернете</label>
-                    <input type="radio" name="source" id="friends" value="friends" 
-                    <?php
-                         if(isset($_GET['E']) && $_GET['E'] == 'friends') {
-                        echo 'checked';
-                         }
-                    ?>
-                    />
-                    <label for="friends">Рассказали друзья</label>
-                </div>
-
-                <div class="form-group">
-                    <label class="form-label" for="category">Категория обращения</label>
-                    <select name="category" id="category">
-              <option value="grievance">Жалоба</option>
-              <option value="proposal">Предложение</option>
-            </select>
-                </div>
-
-                <div class="form-group">
-                    <label class="form-label" for="message">Текст обращения</label>
-                    <textarea name="message" id="message" cols="30" rows="10" placeholder="Введите текст обращения" required>
-                    <?php
-                         if(isset($_GET['MESSAGE'])) {
-                        echo $_GET['MESSAGE'];
-                         } else {
-                        echo '';
-                         }
-                    ?>
-                    </textarea>
-                </div>
-
-                <div class="form-group">
-                    <input type="file" name="attachment" id="attachment" />
-                    <label for="attachment"></label>
-                </div>
-
-                <div class="form-group">
-                    <input type="checkbox" name="agreement" id="agreement" required />
-                    <label for="agreement">Даю согласие на обработку пользовательских данных</label
-            >
-          </div>
-          <div class="button_container">
-            <input class="button_signUp" type="submit" value="Отправить" />
-          </div>
-        </form>
-      </div>
-    </div>
-  </body>
+    </body>
 </html>
+
+
